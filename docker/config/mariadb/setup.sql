50d5883b15fd44d5eb66175a030306f05241ad8b
Raymaekers Luca <raymaekers.luca@gmail.com>
2023-05-12 02:37:05 +0200

    added setup to mariadb server

---
+++ docker/config/mariadb/setup.sql
@@ -0,0 +1,23 @@
+-- create use for phpmyadmin
+CREATE USER 'padmin'@'localhost'
+IDENTIFIED BY 'kirikou';
+
+-- admin account
+GRANT ALL PRIVILEGES ON *.* 
+TO 'padmin'@'localhost' WITH GRANT OPTION;
+
+
+-- From sql secure installation
+-- Remove anonymous user
+DELETE FROM mysql.global_priv WHERE User='';
+
+-- Disable root remote login
+DELETE FROM mysql.global_priv
+WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+
+-- Don't create default 'test' schema
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%'
+
+-- Reload changes
+-- FLUSH PRIVILEGES;
