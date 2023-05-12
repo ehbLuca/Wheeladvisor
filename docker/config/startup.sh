368b86ec5ecc68aebf54aa3f0aa649ac0c122478
Raymaekers Luca <raymaekers.luca@gmail.com>
2023-05-12 02:35:21 +0200

    seperated mariadbd command in script

--- docker/config/startup.sh
+++ docker/config/startup.sh
@@ -3,11 +3,4 @@
 nginx
 
 # This will keep the container running
-/usr/bin/mariadbd \
-	--basedir=/usr \
-	--datadir=/var/lib/mariadb \
-	--plugin-dir=/usr/lib/mariadb/plugin \
-	--user=mysql \
-	--skip-log-error \
-	--pid-file=/run/mysqld/mysqld.pid \
-	--socket=/run/mysqld/mysqld.sock
+tail -f /dev/null
