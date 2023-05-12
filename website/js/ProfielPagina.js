a9a82d3ad0d0e7af9e32aad530aa8aaa1c1e100a
AEBali <133091926+AEBali@users.noreply.github.com>
2023-05-12 16:14:00 +0200

    Javascript Favoriet button proberen schrijven

--- website/js/ProfielPagina.js
+++ website/js/ProfielPagina.js
@@ -1,14 +1,31 @@
 console.log("Test");
 
 /*favorite button aanspreken*/
+const favButton = document.querySelector('.fav-button');
+
+favButton.addEventListener('click', () => {
+/*Send an HTTP POST request to the server with the current page URL as the payload*/
+  fetch('/favorites', {
+    method: 'POST',
+    body: JSON.stringify({url: window.location.href}),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Failed to save favorite');
+    }
+    alert('Page saved as favorite!');
+  })
+  .catch(error => {
+    console.error(error);
+    alert('Failed to save favorite');
+  });
+});
+```
 
 
 
 
 
-
-
-
-if (EventTarget.classlist.contains('favbutton')) {
-    console.log('yes');
-}
