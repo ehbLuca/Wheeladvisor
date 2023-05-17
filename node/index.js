521665084b46e642caa566ff014c948752a312e6
Raymaekers Luca <raymaekers.luca@gmail.com>
2023-05-18 00:25:56 +0200

    added login route

--- node/index.js
+++ node/index.js
@@ -8,7 +8,22 @@ app.get('/', (req, res) => {
 	res.redirect('/index.html')
 });
 
+app.use(express.urlencoded({ extended: true }));
 app.use('/', express.static(path.join(__dirname, 'website')))
+
+// route for logging in
+// expects an email and password
+app.post('/login', async (req, res) => {
+	let email = req.body.email;
+	let password = req.body.password;
+	let result = await loginUser([email, password])
+	if (result)
+	{
+		res.redirect('/login.html');
+	} else {
+		res.redirect('/login-error.html')
+	}
+});
 
 app.listen(port, () => {
 	console.log(`http://localhost:${port}`);
