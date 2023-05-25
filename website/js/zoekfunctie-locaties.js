6e79dc9e0eb0dea985c132a28ab467ec3db920e5
AEBali <133091926+AEBali@users.noreply.github.com>
2023-05-25 09:29:06 +0200

    Javascript veranderen naar backend die data gaat fetchen met hulp van node.js
    Rest is fout, gaat niet werken na feedback & moet allemaal weg.

--- website/js/zoekfunctie-locaties.js
+++ website/js/zoekfunctie-locaties.js
@@ -1,51 +1,9 @@
-const mysql = require('mysql');
-const express = require('express');
-const app = express();
-const port = 3000;
-// maak verbinding met MariaDB-server
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'username',
-    password: 'password',
-    database: 'database_name'
-  });
-  
-// functie om plaatsen te zoeken
-  function searchPlaces(searchQuery) {
-    return new Promise((resolve, reject) => {
-      // Tegen SQL-injecties = SECURITY!
-      const escapedSearchQuery = connection.escape(`%${searchQuery}%`);
-      // SQL query voor naam & categorie
-      const query = `SELECT place.name AS place_name, categories.name AS category_name
-      FROM places
-      JOIN categories ON places.category_id = categories.id
-      WHERE places.name LIKE ${escapedSearchQuery}`;
-      //SQL query als user zoekt voor minimum rating & boven (bv. gaat alle results met 3/5 + boven tonen)
-      if (minRating) {
-        query += ` AND places.id IN (SELECT places_id FROM reviews WHERE rating >= ${minRating})`;
-      }
-      // voer query uit
-      connection.query(query, (error, results) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(results);
-        }
-      });
-    });
-  }
-  
-// voorbeeld: zoeken voor Brussel Nationaal Museum
-  searchPlaces('Brussel Nationaal Museum')
-    .then((results) => {
-      console.log(results);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+async function searchPlaces() {
  
-//Dit voorbeeld gaat ervan uit dat tabel met de naam "places" in MariaDB-database is met een kolom "name" genoemd die de namen van de plaatsen opslaat. 
-//Functie "searchPlaces" neemt parameter "searchQuery", de query die de gebruiker invoert. 
-//Tabel "plaatsen" heeft kolom met naam "category_id" die verwijst naar kolom "id" in tabel "categorieën".
+  const response = await fetch(`/search?query=${query}&category=${category}`);
+  const results = await response.json();
 
-  
+//Gaat asynchrone HTTP-request zenden naar database met 'name' en 'category' als parameters
+//Gaat resultaat assignen aan parameter 'response
+
+}
