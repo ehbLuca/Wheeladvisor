e4b2bc10ab89e35268ee0d9f939fb02033ca54ae
AEBali <133091926+AEBali@users.noreply.github.com>
2023-05-25 10:20:17 +0200

    Functionaliteit toegevoegd aan Javascript pagina
    Event Listener + Input elementen

--- website/js/zoekfunctie-locaties.js
+++ website/js/zoekfunctie-locaties.js
@@ -1,9 +1,26 @@
+const searchForm = document.querySelector('form');
+searchForm.addEventListener('submit', event => {
+  event.preventDefault();
+  searchPlaces();
+});
+//Event Listener die 'searchPlaces' gaat aanspreken wanneer button wordt geklikt. 
+
+
 async function searchPlaces() {
- 
+
+  const searchInput = document.getElementById('searchInput');
+  const categoryInput = document.getElementById('categoryInput');
+  const resultsContainer = document.getElementById('resultsContainer');
+
+  const query = searchInput.value;
+  const category = categoryInput.value;
+
   const response = await fetch(`/search?query=${query}&category=${category}`);
   const results = await response.json();
 
-//Gaat asynchrone HTTP-request zenden naar database met 'name' en 'category' als parameters
-//Gaat resultaat assignen aan parameter 'response
+  resultsContainer.innerHTML = '';
+
+//Gaat asynchrone HTTP-request zenden naar database met zoek queries als parameters
+//Gaat resultaat assignen aan parameter 'response'
 
 }
