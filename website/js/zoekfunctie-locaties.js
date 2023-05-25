7e1b98cc0fdf21ac9c9f0842fae042a3ade62750
AEBali <133091926+AEBali@users.noreply.github.com>
2023-05-25 12:00:27 +0200

    Toevoegen van "rating" search optie

--- website/js/zoekfunctie-locaties.js
+++ website/js/zoekfunctie-locaties.js
@@ -6,17 +6,18 @@ searchForm.addEventListener('submit', event => {
 //Event Listener die 'searchPlaces' gaat aanspreken wanneer button wordt geklikt. 
 
 async function searchPlaces() {
-
   const searchInput = document.getElementById('searchInput');
   const categoryInput = document.getElementById('categoryInput');
+  const minRatingInput = document.getElementById('minRatingInput');
   const resultsContainer = document.getElementById('resultsContainer');
-
+ 
   const query = searchInput.value;
   const category = categoryInput.value;
-
-  const response = await fetch(`/search?query=${query}&category=${category}`);
+  const minRating = minRatingInput.value;
+ 
+  const response = await fetch(`/search?query=${query}&category=${category}&minRating=${minRating}`);
   const results = await response.json();
-
+ 
   resultsContainer.innerHTML = '';
 
   //Gaat asynchrone HTTP-request zenden naar database met zoek queries als parameters
