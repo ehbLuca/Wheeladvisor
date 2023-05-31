66e3f4af000ac6631c2c979ea6686695b8fa2ead
Henrique3040 <henrique.de.sousa.lima.cardoso@student.ehb.be>
2023-05-31 12:02:49 +0200

    favoriet gemaakt.js

---
+++ src/website/js/favoriete.js
@@ -0,0 +1,31 @@
+'use strict'
+// cheack if users are connected before sending a request to the datebase
+
+let buttonFavoriete = document.getElementById('favoriete');
+let user_id = null;
+
+async function getLogin(){
+    return await fetch('/loggedIn').then(
+		result =>  result.json()
+	).catch(
+		error => console.error('Error:', error)
+	);
+}
+
+
+async function func() {
+    user_id = await getLogin();
+    if(user_id)
+    {
+
+       buttonFavoriete.addEventListener("click", function(){
+
+        window.location.href = "/favoriete.html" +  `?id=${user_id}`
+        console.log('click')
+
+       });
+        
+    }
+}
+func();
+
