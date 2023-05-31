3eb6d6cf453ad380939406887fbcc25664c792ff
Raymaekers Luca <raymaekers.luca@gmail.com>
2023-05-31 22:41:49 +0200

    removed remuncant code

--- src/website/js/zoek.js
+++ src/website/js/zoek.js
@@ -19,14 +19,6 @@ async function getPlaces() {
 
 window.addEventListener('DOMContentLoaded', async function() {
 	var contentContainer = document.getElementById('contentContainer');
-	// var isLoading = false;
-	// var page = 1;
-
-		  imageElement.src = `images/places/${place.place_id}`;
-      anchorElement.href="ProfielPagina.html"+`?id=${place.place_id}`;
-		  imageElement.classList.add('post-image');
-		  postElement.classList.add('post');
-		  postElement.textContent = place.name;
 
 	let places = await getPlaces();
 
@@ -57,25 +49,4 @@ window.addEventListener('DOMContentLoaded', async function() {
 		contentContainer.appendChild(anchorElement);
 		anchorElement.appendChild(postElement);
 	}
-
-	// isLoading = false;
-	// page++;
-	// }
-
-	// function isScrollAtBottom() {
-		// var windowHeight = window.innerHeight;
-		// var documentHeight = document.documentElement.scrollHeight;
-		// var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-		// return (windowHeight + 50 + scrollTop >= documentHeight);
-		// }
-
-	// window.addEventListener('scroll', function() {
-		// if (!isLoading && isScrollAtBottom()) {
-			// fetchData(page);
-			// }
-		// });
-
-	// // Initial data fetching
-	// fetchData(page);
 });
