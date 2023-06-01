6444e4e1cafc1232960d734bfa6ba5119c35516b
Henrique3040 <henrique.de.sousa.lima.cardoso@student.ehb.be>
2023-06-01 23:03:07 +0200

    Update app.js

--- src/node/app.js
+++ src/node/app.js
@@ -156,6 +156,19 @@ app.get('/deleteFavorite/user_id/:user_id/place_id/:place_id', async (req, res) 
 });
 
 
+//for the recomandtion
+
+app.get(`/mostFavorite`, function () {
+
+	let result = queries.mostFavorite();
+	console.log
+	return result;
+
+})
+
+
+
+
 app.post('/getplace', async (req, res) => {
 	let place_id = req.body.place_id;
 	let place = await queries.getPlace(place_id);
