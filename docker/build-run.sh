15bc93be9cc74d9b29f1c4e339ade2e3f2f04185
Raymaekers Luca <raymaekers.luca@gmail.com>
2023-06-01 10:35:44 +0200

    use one docker-compose.yml file for both containers

--- docker/build-run.sh
+++ docker/build-run.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 thisdir=$(realpath "$0" | xargs -I {} dirname "{}")
 
-
 for container in mariadb wheeladvisor
 do
 	cd "$thisdir"/$container
 	docker build -t $container . 
-	docker compose up -d
 done
+
+cd "$thidir"
+docker compose up -d
