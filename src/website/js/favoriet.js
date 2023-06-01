c6e1c41418c7b76d227c7b7162739e9d332b713b
Henrique3040 <henrique.de.sousa.lima.cardoso@student.ehb.be>
2023-06-01 02:42:47 +0200

    Update favoriet.js

--- src/website/js/favoriet.js
+++ src/website/js/favoriet.js
@@ -1,14 +1,15 @@
 'use strict'
+
 async function getPlaces() {
-  const urlParams = new URLSearchParams(window.location.search);
-  const query = urlParams.get("q");
+	const urlParams = new URLSearchParams(window.location.search);
+	const user_id = urlParams.get("id");
 	return fetch("/favourite", {
 		method: "POST",
 		headers: {
 			'Content-Type': 'application/json'
 		},
 		body: JSON.stringify({
-			q: query
+			user_id
 		})
 	}).then(
 		result => result.json()
@@ -18,6 +19,7 @@ async function getPlaces() {
 }
 
 window.addEventListener('DOMContentLoaded', async function() {
+<<<<<<< HEAD
   var contentContainer = document.getElementById('contentContainer');
   // var isLoading = false;
   // var page = 1;
@@ -41,30 +43,38 @@ window.addEventListener('DOMContentLoaded', async function() {
 		  imageElement.classList.add('post-image');
 		  postElement.classList.add('post');
 		  postElement.textContent = place.name;
+=======
+	var contentContainer = document.getElementById('contentContainer');
+	let places = await getPlaces();
+	if (!places)
+		return
 
-		  postElement.appendChild(imageElement);
-		  contentContainer.appendChild(anchorElement);
-		  anchorElement.appendChild(postElement);
-      }
-  
-      // isLoading = false;
-      // page++;
-  // }
-  
-  // function isScrollAtBottom() {
-    // var windowHeight = window.innerHeight;
-    // var documentHeight = document.documentElement.scrollHeight;
-    // var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  
-    // return (windowHeight + 50 + scrollTop >= documentHeight);
-  // }
-  
-  // window.addEventListener('scroll', function() {
-    // if (!isLoading && isScrollAtBottom()) {
-      // fetchData(page);
-    // }
-  // });
-  
-  // // Initial data fetching
-  // fetchData(page);
+	for (let place of places) {
+		console.log(place)
+		var anchorElement = document.createElement('a');
+		var postElement = document.createElement('div');
+		var imageElement = document.createElement('img');
+		let fileURL = `images/places/${place.place_id}`;
+		let imageExist = await fetch (fileURL, { method: 'HEAD' })
+			.then(response => {
+				return response.ok; // Returns true if the file exists, false otherwise
+			})
+			.catch(() => {
+				return false; // Error occurred, file does not exist
+			});
+>>>>>>> 44e7cedbdead13f830f95338c64f69d60119690e
+
+		if (imageExist) 
+			imageElement.src = fileURL;
+		else
+			imageElement.src = 'images/fallbackfoto.png';
+		anchorElement.href = `plekpagina.html?id=${place.place_id}`
+		imageElement.classList.add('post-image');
+		postElement.classList.add('post');
+		postElement.textContent = place.name;
+
+		postElement.appendChild(imageElement);
+		contentContainer.appendChild(anchorElement);
+		anchorElement.appendChild(postElement);
+	}
 });
